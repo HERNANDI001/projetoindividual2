ba2e1bd9c4e1a243c766a1ff1fc434f3b05f223e
Hernandi de Assis <145159351+HERNANDI001@users.noreply.github.com>
2023-10-21 14:28:53 -0300

    Update projeto individual.py

--- projeto individual.py
+++ projeto individual.py
@@ -1,82 +1,51 @@
 
 
 
-# Função para obter a avaliação de uma etapa do processo
-def obter_avaliacao(etapa):
-    try:
-        avaliacao = float(input(f"Digite a avaliação para {etapa}: "))
-        return avaliacao
-    except ValueError:
-        print("Por favor, insira um valor numérico.")
-        return obter_avaliacao(etapa)
+# Lista para os resultados dos candidatos
+candidatos = []
 
-# Lista de etapas do processo
-etapas = ["entrevista", "teste teórico", "teste prático", "avaliação de soft skills"]
+def calcularmedia(entrevista, teorico, pratico, soft_skills):
+    return (entrevista + teorico + pratico + soft_skills) / 4
 
-# Inicializa um dicionário para armazenar as avaliações
-avaliacoes = {}
+def avaliaraprovacao(media):
+    return "Aprovado" if media >= 7 else "Reprovado"
 
-# Coleta as avaliações para cada etapa do processo
-for etapa in etapas:
-    avaliacao = obter_avaliacao(etapa)
-    avaliacoes[etapa] = avaliacao
+def adicionarcandidato():
+    nome = str(input("Nome do candidato: "))
+    entrevista = float(input("Nota da entrevista: "))
+    teorico = float(input("Nota do teste teórico: "))
+    pratico = float(input("Nota do teste prático: "))
+    soft_skills = float(input("Nota de soft skills: "))
 
-# Constrói a string no formato desejado
-resultado = "e{}_t{}_p{}_s{}".format(
-    avaliacoes["entrevista"],
-    avaliacoes["teste teórico"],
-    avaliacoes["teste prático"],
-    avaliacoes["avaliação de soft skills"]
-)
+    media = calcularmedia(entrevista, teorico, pratico, soft_skills)
+    resultado = avaliaraprovacao(media)
 
-# Imprime a string formatada
-print("Resultado: " + resultado)
+    candidato = {
+        "Nome": nome,
+        "Entrevista": entrevista,
+        "Teórico": teorico,
+        "Prático": pratico,
+        "Soft Skills": soft_skills,
+        "Média": media,
+        "Resultado": resultado
+    }
 
+    candidatos.append(candidato)
 
+def buscar_candidato(nome):
+    for candidato in candidatos:
+        if candidato["Nome"] == nome:
+            print(f"Nome: {candidato['Nome']}")
+            print(f"Entrevista NOTA {candidato['Entrevista']}, Teoria NOTA {candidato['Teórico']}, Prática NOTA {candidato['Prático']}, Soft NOTA {candidato['Soft Skills']}")
+            print(f"Resultado: {candidato['Resultado']}")
+            return
 
-# Função para coletar avaliações de um candidato
-def coletar_avaliacoes():
-    avaliacoes = {}
-    etapas = ["entrevista", "teste teórico", "teste prático", "avaliação de soft skills"]
-    for etapa in etapas:
-        avaliacao = float(input(f"Digite a avaliação para {etapa}: "))
-        avaliacoes[etapa] = avaliacao
-    return avaliacoes
+continuar = True
 
-# Função para adicionar um candidato à lista
-def adicionar_candidato(lista, nome, avaliacoes):
-    candidato = f"{nome}: e{avaliacoes['entrevista']}_t{avaliacoes['teste teórico']}_p{avaliacoes['teste prático']}_s{avaliacoes['avaliação de soft skills']}"
-    lista.append(candidato)
+while continuar:
+    adicionarcandidato()
+    continuarresposta = input("Deseja continuar inserindo candidatos (sim/não)? ").lower()
+    continuar = continuarresposta == "sim"
 
-# Função para buscar candidatos com base nos critérios
-def buscar_candidatos(lista, critrios):
-    candidatos_selecionados = []
-    for candidato in lista:
-        if all(crit in candidato for crit in critrios):
-            candidatos_selecionados.append(candidato)
-    return candidatos_selecionados
-
-# Lista para armazenar os resultados
-resultados = []
-
-# Coletar informações do candidato
-nome = input("Digite o nome do candidato: ")
-avaliacoes = coletar_avaliacoes()
-
-# Adicionar candidato à lista
-adicionar_candidato(resultados, nome, avaliacoes)
-
-# Solicitar critérios de busca ao usuário
-critrios = input("Digite os critérios de busca (por exemplo, 'eX_tX' para buscar candidatos com essas avaliações): ").split('_')
-
-# Realizar a busca
-candidatos_selecionados = buscar_candidatos(resultados, critrios)
-
-# Exibir os candidatos que atendem aos critérios
-if candidatos_selecionados:
-    print("Candidatos encontrados:")
-    for candidato in candidatos_selecionados:
-        print(candidato)
-else:
-    print("Nenhum candidato encontrado com os critérios especificados.")
-
+nomebuscar = input("Digite o nome do candidato que deseja buscar: ")
+buscar_candidato(nomebuscar)
